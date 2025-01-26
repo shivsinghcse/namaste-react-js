edbde0a81e326bd2e0f2bdc0a584f57114d5c220
Shiv Singh <shivsinghcse@gmail.com>
2025-01-27 01:22:26 +0530

    Hello world using JavaScript

---
+++ Episode_01_Inception/HelloWorld_Js/app.js
@@ -0,0 +1,8 @@
+  // creating a heading tag
+  const heading = document.createElement('h1');
+  // adding text to heading
+  heading.innerText = "Hello World from JavaScript😎";
+  // selecting root element 
+  const root = document.getElementById('root');
+  // insrting heading to root 
+  root.appendChild(heading);
