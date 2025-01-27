2fa374361d506c77a8b0e4d22b1a9812db667fa4
Shiv Singh <shivsinghcse@gmail.com>
2025-01-28 00:09:29 +0530

    JSX

--- food-app/App.js
+++ food-app/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', {}, 'Hello World from React🚀😎');
+// React Element
+
+const heading = React.createElement('h1', null, 'Hello World From React🚀😎');
+
+console.log(heading);
+
+// JSX
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel
+// JSX => Babel transpile it to React.createElement => ReactElement - JS Object => HTMLElement(render)
+
+const jsxHeading = (
+    <h1 className="heading" tabIndex="3">
+        Hello World from JSX🚀
+    </h1>
+);
+
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+
+root.render(jsxHeading);
