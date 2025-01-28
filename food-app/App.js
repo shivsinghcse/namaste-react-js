6976739a9f02253ab4925f3c7da8eb0682a0bed0
Shiv Singh <shivsinghcse@gmail.com>
2025-01-28 14:19:09 +0530

    created restaurant card

--- food-app/App.js
+++ food-app/App.js
@@ -9,6 +9,8 @@ import ReactDOM from 'react-dom/client';
  *  - Search
  *  - RestaurantContainer
  *      - RestaurantCard
+ *          - Img
+ *          - Name of Res, Star rating, cuisine, delivery time
  *  Footer
  *  - Copyright
  *  - Links
@@ -25,7 +27,7 @@ const Header = () => {
                     src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
                 />
             </div>
-            <div className='nav-items'>
+            <div className="nav-items">
                 <ul>
                     <li>Home</li>
                     <li>About us</li>
@@ -37,10 +39,85 @@ const Header = () => {
     );
 };
 
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img
+                className="res-logo"
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ebfc6a45deec38c12d1acd23fe9a8cef"
+                alt="res-logo"
+                style={{
+                    width: '100%',
+                    height: '65%',
+                }}
+            />
+            <div className="res-details" style={{
+                lineHeight: 1.4,
+                paddingLeft: '10px',
+            }}>
+                <h3 style={{ marginTop: '5px' }}>Punjabi Dhaba</h3>
+                <h4
+                    style={{
+                        fontWeight: '600',
+                        color: '#676A6D',
+                    }}
+                >
+                    <span
+                        style={{
+                            color: 'green',
+                        }}
+                    >
+                        &#9733;
+                    </span>{' '}
+                    4.3 | 25-50 mins
+                </h4>
+                <h4
+                    style={{
+                        fontWeight: '500',
+                        color: '#676A6D',
+                    }}
+                >
+                    Thalis, North Indian, Biryani
+                </h4>
+                <h4
+                    style={{
+                        fontWeight: 'normal',
+                        color: '#676A6D',
+                    }}
+                >
+                    Aliganj
+                </h4>
+            </div>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container" >
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    );
+};
+
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
+            <Body />
         </div>
     );
 };
