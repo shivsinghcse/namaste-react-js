3e9ca149e252bdcac4beedfae122b454352307f0
Shiv Singh <shivsinghcse@gmail.com>
2025-01-29 01:00:14 +0530

    cleaning and structreing our app

---
+++ food-app/src/Components/Body.js
@@ -0,0 +1,22 @@
+import RestaurantCard from './Restaurant';
+import resList from '../../utils/mockData';
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {resList.map((restaurant) => {
+                    return (
+                        <RestaurantCard
+                            key={restaurant.info.id}
+                            resData={restaurant}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Body;
---
+++ food-app/src/Components/Restaurant.js
@@ -0,0 +1,72 @@
+import { CDN_URL } from "../../utils/constants";
+
+const RestaurantCard = (props) => {
+    const { resData } = props;
+
+    const {
+        name,
+        avgRating,
+        costForTwo,
+        cuisines,
+        areaName,
+        cloudinaryImageId,
+    } = resData?.info;
+
+    return (
+        <div className="res-card">
+            <img
+                className="res-logo"
+                src={CDN_URL + cloudinaryImageId}
+                alt="res-logo"
+                style={{
+                    width: '100%',
+                    height: '65%',
+                }}
+            />
+            <div
+                className="res-details"
+                style={{
+                    lineHeight: 1.4,
+                    paddingLeft: '10px',
+                }}
+            >
+                <h3 style={{ marginTop: '5px' }}>{name}</h3>
+                <h4
+                    style={{
+                        fontWeight: '600',
+                        fontSize: '14px',
+                        color: '#676A6D',
+                    }}
+                >
+                    <span
+                        style={{
+                            color: 'green',
+                        }}
+                    >
+                        &#9733;
+                    </span>
+                    {' ' + avgRating} | {resData.info.sla.slaString} |{' '}
+                    {costForTwo}
+                </h4>
+                <h4
+                    style={{
+                        fontWeight: '500',
+                        color: '#676A6D',
+                    }}
+                >
+                    {cuisines.join(', ')}
+                </h4>
+                <h4
+                    style={{
+                        fontWeight: 'normal',
+                        color: '#676A6D',
+                    }}
+                >
+                    {areaName}
+                </h4>
+            </div>
+        </div>
+    );
+};
+
+export default RestaurantCard;
