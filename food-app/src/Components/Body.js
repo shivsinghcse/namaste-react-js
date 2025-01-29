767d504137fe04d53f3fa9ea535a742c1c828333
Shiv Singh <shivsinghcse@gmail.com>
2025-01-29 12:30:35 +0530

    updating UI with useState()

--- food-app/src/Components/Body.js
+++ food-app/src/Components/Body.js
@@ -1,12 +1,29 @@
 import RestaurantCard from './Restaurant';
 import resList from '../../utils/mockData';
+import { useState } from 'react';
 
 const Body = () => {
+    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            {/* <div className="search">Search</div> */}
+            <div className="top-rated">
+                <button
+                    className="top-rated-btn"
+                    onClick={() => {
+                        // filter logic
+                        const filteredList = listOfRestaurant.filter((res) => {
+                            return res.info.avgRating > 4.5;
+                        });
+                        setListOfRestaurant(filteredList);
+                    }}
+                >
+                    Top Rated Restaurant
+                </button>
+            </div>
             <div className="res-container">
-                {resList.map((restaurant) => {
+                {listOfRestaurant.map((restaurant) => {
                     return (
                         <RestaurantCard
                             key={restaurant.info.id}
